5a918e045291a1795b718fc0b22b8fb5a3f564ca
songxingguo <13289899422.qq.com>
2020-01-17 16:33:49 +0800

    feat:  去除代理

--- vue.config.js
+++ vue.config.js
@@ -6,32 +6,5 @@ module.exports = {
     port: 8080,//端口号
     https: false,//true启动浏览器会给出一些警告
     hotOnly: false,//热更新 webpack已经有
-    proxy: {
-      // 配置跨域，请求后端接口
-      '/api': {
-        target: 'https://api.weixin.qq.com',//源地址
-        changeOrigin: true,//是否允许跨域
-        ws: true,//是否代理websockets
-        pathRewrite: {
-          '^/api': ''
-        }
-      },
-      '/foo': {
-        target: 'https://cos.ap-shanghai.myqcloud.com',//源地址
-        changeOrigin: true,//是否允许跨域
-        ws: true,//是否代理websockets
-        pathRewrite: {
-          '^/foo': ''
-        }
-      },
-      '/xfyun': {
-        target: 'https://api.xfyun.cn',//源地址
-        changeOrigin: true,//是否允许跨域
-        ws: true,//是否代理websockets
-        pathRewrite: {
-          '^/xfyun': ''
-        }
-      }
-    }
   }
 }
